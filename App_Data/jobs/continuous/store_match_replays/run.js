44c0a22846b76bcf853df3b14a847c6a125e419f
Peter Eliasson <warackta@gmail.com>
2015-06-07 15:32:44 +0200

    Removed dist folder, as heroku can run gulp.

--- App_Data/jobs/continuous/store_match_replays/run.js
+++
@@ -1,4 +0,0 @@
-// Run compiled job from dist folder
-require('../../../../dist/jobs/continuous/store_match_replays');
-
-// ..
--- App_Data/jobs/triggered/add_matches_to_replay_queue/run.js
+++
@@ -1,4 +0,0 @@
-// Run compiled job from dist folder
-require('../../../../dist/jobs/triggered/add_matches_to_replay_queue');
-
-// ..
